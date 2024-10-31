7e0dc1cb3a808c8d123f58b7934e420f7ea8e8a3
kellen-martin <kmartin6@iastate.edu>
2024-10-31 15:35:36 -0500

    add newton method of finding angles to f of a evaluation

--- Testing.m
+++ Testing.m
@@ -22,12 +22,6 @@ alpha = 6378;       % [km]
 % angle between r1 & r2
 theta = dot(r1, r2)/(norm(r1)*norm(r2));
 
-% direction of the angular momentum vecotr
-h_hat = cross(r1,r2)/norm(cross(r1,r2));
-
-% s = sin^2(i)
-s = h_hat(3)^2;
-
 % cord length
 c = norm(r1 - r2);
 
@@ -50,8 +44,8 @@ sum1 = 0;
 sum2 = 0;
 for j=1:(N-1)
     C_j = a0 + Cr*exp(pi*1i*j/N);
-    sum1 = sum1 + exp(2*pi*1i*j/N)*f_eval(theta, mu, J_2, alpha, s, c, C_j, semi, r1, r2);
-    sum2 = sum2 + exp(pi*1i*j/N)*f_eval(theta, mu, J_2, alpha, s, c, C_j, semi, r1, r2);
+    sum1 = sum1 + exp(2*pi*1i*j/N)*f_eval(theta, mu, J_2, alpha, c, C_j, semi, r1, r2);
+    sum2 = sum2 + exp(pi*1i*j/N)*f_eval(theta, mu, J_2, alpha, c, C_j, semi, r1, r2);
 end
 
 f_plus = f_eval(theta, mu, J_2, alpha, s, c, a0 + Cr, semi, r1, r2);
@@ -115,10 +109,11 @@ function [i, Omega_1, Omega_2] = newton_angles(r1_vec, r2_vec, a, e, J2, mu, del
     end
 end
 
-function val = f_eval(theta, mu, J2, alpha, s, c, a, semi, r1, r2)
+function val = f_eval(theta, mu, J2, alpha, c, a, semi, r1, r2)
     % find the angles
     [psi, cphi] = get_angles(semi, c, a);
     e = get_eccentricity(r1, r2, theta, psi, a);
-    val = sqrt(mu/(a^3))/(2*(psi - sin(psi)*cphi) + (J2*((alpha/(2*a))^2)*(3*s - 2))/(((1 - e^2)^3))*(4*(e^2 + 2)*psi - 16*sin(psi)*cphi + 4*sin(2*psi)*(cphi^2 - e^2/2)));
+    [i,~,~] = newton_angles(r1, r2, a, e, J2, mu, delta_t);
+    val = sqrt(mu/(a^3))/(2*(psi - sin(psi)*cphi) + (J2*((alpha/(2*a))^2)*(3*sin(i)^2 - 2))/(((1 - e^2)^3))*(4*(e^2 + 2)*psi - 16*sin(psi)*cphi + 4*sin(2*psi)*(cphi^2 - e^2/2)));
 end
 
