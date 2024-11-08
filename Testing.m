0667ffb25d47b70ce7f463802406988184a3e417
Kellen-Martin <kmartin6@iastate.edu>
2024-11-08 15:54:55 -0600

    commit

--- Testing.m
+++ Testing.m
@@ -50,6 +50,8 @@ z = alpha*z;
 surf(x,y,z)
 hold off
 
+%% Test thing
+[i, Omega_1, Omega_2] = newton_angles_2(r1, r2, a_prop, e_prop, J_2, mu, alpha, delta_t)
 %% Compute other quantites
 % angle between r1 & r2
 theta = dot(r1, r2)/(norm(r1)*norm(r2));
@@ -131,7 +133,7 @@ function [i, Omega_1, Omega_2] = newton_angles_2(r1_vec, r2_vec, a, e, J2, mu, a
     F = @(x) [sin(x(1))*cos(x(2))*r1_vec(1) + sin(x(1))*sin(x(2))*r1_vec(2) + cos(x(1))*r1_vec(3);
               sin(x(1))*cos(x(3))*r2_vec(1) + sin(x(1))*sin(x(3))*r2_vec(2) + cos(x(1))*r2_vec(3);
               x(2) - Beta*cos(x(1))*delta_t - x(3)];
-    x = fsolve(F, [0, 0, 0]);
+    x = fsolve(F, [.5, 0, 0]);
 
     i = x(1);
     Omega_1 = x(2);
