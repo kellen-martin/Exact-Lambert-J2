c44f8ae62e2a5f7c1a3dfe01bfce3e3287b55b80
Kellen-Martin <kmartin6@iastate.edu>
2025-02-13 12:04:11 -0600

    clean up

--- Lambert_J2_MonteCarlo.m
+++ Lambert_J2_MonteCarlo.m
@@ -13,40 +13,39 @@ J_2 = 1.0826E-3;
 alpha = 6378;       % [km]
 
 %% Monte Carlo
-N = 64;
-% a_error_mean = zeros(1, length(N));
-% v1_error_mean = zeros(1, length(N));
-% v2_error_mean = zeros(1, length(N));
-% solve_time_mean = zeros(1, length(N));
-
-for j=1:length(N)
-% error_v1_rel = zeros(1,100);
-% error_v2_rel = zeros(1,100);
-% % error_a_rel = zeros(1,100);
-% solve_time = zeros(1,100);
-% delta_t = zeros(1,100);
-
-    for i=1:100
+N = 500;
+trials = 100;
+error_v1_rel = zeros(1,trials);
+error_v2_rel = zeros(1,trials);
+error_a_rel = zeros(1,trials);
+delta_t = zeros(1,trials);
+oes = zeros(6,trials);
+h = waitbar(0, 'Running');
+    for i=1:trials
     % Create Random Initial Conditions
     
-    [r1, v1, r2, v2, delta_t(i)] = lambert_conditions_lopez();
+    [r1, v1, r2, v2, delta_t(i), a_L] = lambert_conditions_lopez();
+    [a2(i), e2(i), p2(i), i2(i), Omega2(i), omega2(i), f2(i)] = get_oe(r2, v2, mu);
     
     % Lambert Solve
-    tic
-    [a, v1_L, v2_L] = Lamabert_J2_1newt(r1, r2, delta_t(i), mu, J_2, alpha, N(j));
-    solve_time(i) = toc;
-    
+    [a, v1_L, v2_L] = Lamabert_J2_1newt(r1, r2, delta_t(i), mu, J_2, alpha, N);
+
     % Errors
+    error_a_rel(i) = abs(a_L - a)/a_L;
     error_v1_rel(i) = norm(v1_L - v1)/norm(v1);
     error_v2_rel(i) = norm(v2_L - v2)/norm(v2);
+    waitbar(i/trials, h)
     end
 
 % Mean Error
-% a_error_mean(j) = mean(error_a_rel);
-v1_error_mean(j) = mean(error_v1_rel);
-v2_error_mean(j) = mean(error_v2_rel);
-solve_time_mean(j) = mean(solve_time);
-end
+a_error_mean = mean(error_a_rel);
+v1_error_mean = mean(error_v1_rel);
+v2_error_mean = mean(error_v2_rel);
+
+% Standard Deviation of the Error
+v1_error_std = std(error_v1_rel);
+v2_error_std = std(error_v2_rel);
+
 %% Plots
 
 % Error Plots
